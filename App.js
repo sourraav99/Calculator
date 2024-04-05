49bf674d49a36b3451840e24cc80e11ea1ba8fb8
Sourabh singh <149767822+sourraav99@users.noreply.github.com>
2024-04-05 20:25:42 +0530

    added file to github
    this is a calculator app

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{justifyContent:'center',alignItems:"center",flex:1}}>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
